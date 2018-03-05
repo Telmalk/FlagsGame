d4f801c2cc5f95f0ff22eace1f6a27c81eb2a56a
Clément Haller <clement.haller@hetic.net>
2018-03-05 12:28:56 +0100

    core du jeu

--- Flags/app.js
+++ Flags/app.js
@@ -3,6 +3,9 @@ var startScreen = document.querySelector(".screen");
 var timer = document.querySelector(".time span");
 var gameOver = document.querySelector("#game-over");
 var flag = document.querySelectorAll('.flag');
+var score = document.querySelector(".score strong");
+var imgLife = document.querySelectorAll(".lives img");
+var life = 2;
 
 var time = function() {
     if (timer.textContent > 0)
@@ -12,17 +15,66 @@ var time = function() {
 }
 
 var startTimer = function() {
-    console.log("yep")
-    intervalID = setInterval(time, 1000)
+    intervalID = setInterval(time, 1000);
+    console.log(flag);
 }
 
 var rand = function(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+var generateRandomFlags = function(indexFlag) {
+    var sameColors = [];
+    var goodColors = flags[indexFlag].colors;
+
+    for (var i = 0; i < flags.length; i++) {
+        var isPush = 0;
+        for (var j = 0; j < goodColors.length; j++) {
+            for (var k = 0; k < flags[i].colors.length; k++) {
+                if (isPush === 0 && i !== indexFlag) {
+                    if (flags[i].colors[k] === goodColors[j]) {
+                        sameColors.push(flags[i]);
+                        isPush = 1;
+                    }
+                }
+            }
+        }
+    }
+  return sameColors;
+}
+
+var copyTab = function(deletePos, oldTab) {
+    var newTab = []
+
+    for (var i = 0; i < oldTab.length; i++) {
+        if (i !== deletePos)
+            newTab.push(oldTab[i]);
+    }
+    return newTab;
+}
+var generatePositionFlag = function(goodPlace, goodColors, imgFlag) {
+    var i = 0;
+
+    while (i < imgFlag.length) {
+        var randWrongFlag = rand(goodColors.length);
+        if (i !== goodPlace) {
+            imgFlag[i].src = "flags/" + goodColors[randWrongFlag].code.toLowerCase() + ".svg";
+            imgFlag[i].dataset.code = "no"
+            goodColors = copyTab(randWrongFlag, goodColors);
+        }
+        i++;
+    }
+}
+
+var clear = function(clearFlags) {
+    for (var i = 0; i < clearFlags.length; i++)
+        clearFlags[i].src = "";
+}
+
 var generateGame = function() {
     var imgFlag = document.querySelectorAll('.flags img');
     var contryName = document.querySelector("h2");
+    clear(imgFlag);
     var indexFlag = rand(flags.length);
     var selectFlag = flags[indexFlag];
     var randomPos = rand(4);
@@ -30,21 +82,39 @@ var generateGame = function() {
     contryName.textContent = flags[indexFlag].name;
     imgFlag[randomPos].src = "flags/" + selectFlag.code.toLowerCase() + ".svg";
     imgFlag[randomPos].dataset.code = flags[indexFlag].code;
+    var goodColors = generateRandomFlags(indexFlag);
+    generatePositionFlag(randomPos, goodColors, imgFlag);
 }
 
 for (let i = 0; i < start.length; i++) {
     start[i].addEventListener("click", function() {
         startScreen.classList.remove("is-open");
         gameOver.classList.remove('is-open')
-        timer.textContent = 1000;
+        timer.textContent = 20;
+        life = 2;
+        score.textContent = 0;
         generateGame();
     });
 }
 
 
-for (let i = 0; i < flag.length; i++) {
-    flag[i].addEventListener("click", function() {
-        console.log(flag[i]);
+var flagImg = document.querySelectorAll(".flags img")
+for (let i = 0; i < flagImg.length; i++) {
+    flagImg[i].addEventListener("click", function() {
+        if (flagImg[i].dataset.code !== "no") {
+            generateGame();
+            score.textContent = parseInt(score.textContent) + 10;
+            if (timer.textContent < 27)
+                timer.textContent = parseInt(timer.textContent) + 3;
+            else
+                timer.textContent = 30;
+        } else  {
+            imgLife[life].style.color = "white";
+            life--;
+            if (life === -1) {
+                gameOver.classList.add('is-open');
+            }
+            console.log(life);
+        }
     });
 }
-
